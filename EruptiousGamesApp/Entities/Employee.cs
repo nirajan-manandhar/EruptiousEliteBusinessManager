3e73c702fcee2a5f2b8ed955d45ad733c1e2c0ff
Hank <cchang189@my.bcit.ca>
2019-05-06 11:52:56 -0700

    SimpleDB

---
+++ EruptiousGamesApp/Entities/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EruptiousGamesApp.Entities
+{
+    public enum Role
+    {
+        AMBASSADOR, MANAGER, ADMIN
+    }
+
+    public enum EmpStatus
+    {
+        ACTIVE, INACTIVE
+    }
+
+    public class Employee
+    {
+        [Key]
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public Role Role { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public EmpStatus EmpStatus { get; set; }
+
+    }
+}
