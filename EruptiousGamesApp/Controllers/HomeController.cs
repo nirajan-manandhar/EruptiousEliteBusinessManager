99b06596044c8c2a8256829a9158561f62ef8ebc
AskaFang17 <32403190+AskaFang17@users.noreply.github.com>
2019-05-06 22:25:05 -0700

    commit

--- EruptiousGamesApp/Controllers/HomeController.cs
+++ EruptiousGamesApp/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace EruptiousGamesApp.Controllers
 {
     public class HomeController : Controller
     {
-        [Authorize]
         public ActionResult Index()
         {
             return View();
