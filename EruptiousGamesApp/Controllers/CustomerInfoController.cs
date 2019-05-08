9014fb3a1488bf033fdeb27c9201285ec404fa1d
nirajan-manandhar <manandnirajan@gmail.com>
2019-05-08 16:42:00 -0700

    Added Customer Info Page, before available to connect to database

--- EruptiousGamesApp/Controllers/CustomerInfoController.cs
+++ EruptiousGamesApp/Controllers/CustomerInfoController.cs
@@ -19,8 +19,39 @@ namespace EruptiousGamesApp.Controllers
         // GET: CustomerInfo
         public ActionResult Index()
         {
-            var customers = db.Customers.Include(c => c.Campaign).Include(c => c.Employee);
-            return View(customers.ToList());
+            //ViewBag.CamID = new SelectList(db.Campaigns, "CamID", "CamName");
+            //ViewBag.EmpID = new SelectList(db.Employees, "EmpID", "EmpName");
+
+            ViewBag.CamID = 1;
+            ViewBag.EmpID = 1;
+
+            return View();
+        }
+
+        //POST: CustomerInfo
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustID,CamID,EmpID,DateTime,CustName,Email,Phone,City,Age,Gender,PTCheck")] Customer customer)
+        {
+
+            var dummyCamID = 1;
+            var dummyEmpID = 1;
+            var dummyDateTime = DateTime.Now;
+
+            customer.CamID = dummyCamID;
+            customer.EmpID = dummyEmpID;
+            customer.DateTime = dummyDateTime;
+
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CamID = new SelectList(db.Campaigns, "CamID", "CamName", customer.CamID);
+            ViewBag.EmpID = new SelectList(db.Employees, "EmpID", "EmpName", customer.EmpID);
+            return View(customer);
         }
     }
 }
