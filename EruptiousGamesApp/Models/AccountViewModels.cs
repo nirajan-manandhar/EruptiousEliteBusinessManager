a7390cf027f7cdb3e89ae5d45e69e1e6d50713cf
Hank <cchang189@my.bcit.ca>
2019-05-19 16:12:44 -0700

    Deleted unnecessary codes

--- EruptiousGamesApp/Models/AccountViewModels.cs
+++ EruptiousGamesApp/Models/AccountViewModels.cs
@@ -4,49 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EruptiousGamesApp.Models
 {
-    //public class ExternalLoginConfirmationViewModel
-    //{
-    //    [Required]
-    //    [Display(Name = "Email")]
-    //    public string Email { get; set; }
-    //}
-
-    //public class ExternalLoginListViewModel
-    //{
-    //    public string ReturnUrl { get; set; }
-    //}
-
-    public class SendCodeViewModel
-    {
-        public string SelectedProvider { get; set; }
-        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
-        public string ReturnUrl { get; set; }
-        public bool RememberMe { get; set; }
-    }
-
-    public class VerifyCodeViewModel
-    {
-        [Required]
-        public string Provider { get; set; }
-
-        [Required]
-        [Display(Name = "Code")]
-        public string Code { get; set; }
-        public string ReturnUrl { get; set; }
-
-        [Display(Name = "Remember this browser?")]
-        public bool RememberBrowser { get; set; }
-
-        public bool RememberMe { get; set; }
-    }
-
-    //public class ForgotViewModel
-    //{
-    //    [Required]
-    //    [Display(Name = "Email")]
-    //    public string Email { get; set; }
-    //}
-
+      
     public class LoginViewModel
     {
         [Required]
@@ -62,26 +20,8 @@ namespace EruptiousGamesApp.Models
         public bool RememberMe { get; set; }
     }
 
-    //public enum Role
-    //{
-    //    AMBASSADOR, MANAGER, ADMIN
-    //}
-
-    //public enum EmpStatus
-    //{
-    //    ACTIVE, INACTIVE
-    //}
-
     public class RegisterViewModel
     {
-        //[Required]
-        //[Display(Name = "Employe Name")]
-        //public string EmpName { get; set; }
-
-        //[Required]
-        //[Display(Name = "Role")]
-        //public Role Role { get; set; }
-
         [Required]
         [Display(Name = "UserName")]
         public string UserName { get; set; }
@@ -99,15 +39,6 @@ namespace EruptiousGamesApp.Models
 
         [Required]
         public Employee Employee { get; set; }
-
-        //public EmpStatus EmpStatus { get; set; }
-
-
-        //public virtual ICollection<Campaign> Campaigns { get; set; }
-        //public virtual ICollection<Work> Workings { get; set; }
-        //public virtual ICollection<Customer> Customers { get; set; }
-        //public virtual ICollection<Note> Notes { get; set; }
-        //public virtual ICollection<Request> Requests { get; set; }
     }
 
     public class ChangePasswordModel
@@ -127,32 +58,4 @@ namespace EruptiousGamesApp.Models
     }
 
 
-    //public class ResetPasswordViewModel
-    //{
-    //    [Required]
-    //    [EmailAddress]
-    //    [Display(Name = "Email")]
-    //    public string Email { get; set; }
-
-    //    [Required]
-    //    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-    //    [DataType(DataType.Password)]
-    //    [Display(Name = "Password")]
-    //    public string Password { get; set; }
-
-    //    [DataType(DataType.Password)]
-    //    [Display(Name = "Confirm password")]
-    //    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-    //    public string ConfirmPassword { get; set; }
-
-    //    public string Code { get; set; }
-    //}
-
-    //public class ForgotPasswordViewModel
-    //{
-    //    [Required]
-    //    [EmailAddress]
-    //    [Display(Name = "Email")]
-    //    public string Email { get; set; }
-    //}
 }
